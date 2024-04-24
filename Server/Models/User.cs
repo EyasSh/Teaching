dc36755822aab2269128027f4de12280697fc121
Eyas Sh <eyas.wt@gmail.com>
2024-04-24 21:29:07 +0300

    ignored files

---
+++ Server/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+namespace Server.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; init; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; init; }
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d).{8,}$", ErrorMessage = "Password must contain letters and numbers")]
+        public string Password { get; set; }
+    }
+}
