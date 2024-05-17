ffedfdcf453273a052b83bb321ab17ad8692e9ec
Eyas Sh <eyas.wt@gmail.com>
2024-05-17 16:09:54 +0300

    Finished designing login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Login from './Components/Login/Login';
 
 function App() {
   return (
-     <Login />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </Router>
+     
   );
 }
 
