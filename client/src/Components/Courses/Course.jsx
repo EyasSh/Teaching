e1c242f4f4f1e76de3e2b1935d9e7787f5c09a74
Eyas Sh <eyas.wt@gmail.com>
2024-05-25 22:35:31 +0300

    edited nav style &
    added available courses component

--- client/src/Components/Courses/Course.jsx
+++ client/src/Components/Courses/Course.jsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import "./courses.css"
-import Button from '../Button/Button'
+import React, { useState, useEffect } from 'react';
+import "./courses.css";
+import Button from '../Button/Button';
+import fallbackImage from '../../assets/Images/Fail.jpg'; // Ensure the path is correct
+
 function CourseCard(props) {
-    return (
-        <div className='cont'>
-          
-                <img className='responsive-image' 
-                src={props.image} alt=''
-                />
-           
-                <h3>Xamarin</h3>
-                <section>An extensive MAUI Course in C#</section>
-                <Button className="but" content="View Corse" 
-                link="http://localhost:3000/signup"
-                status='action' 
-                />
-            
-            
-        </div>
-    );
+  const [imageSrc, setImageSrc] = useState(props.image);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await fetch(props.image);
+        if (response.ok) {
+          setImageSrc(props.image);
+        } else {
+          setImageSrc(fallbackImage);
+        }
+      } catch (error) {
+        setImageSrc(fallbackImage);
+      }
+    };
+
+    fetchImage();
+  }, [props.image]);
+
+  const handleImageError = () => {
+    setImageSrc(fallbackImage);
+  };
+
+  return (
+    <div className='cont'>
+      <img
+        className='responsive-image'
+        src={imageSrc}
+        alt='Course'
+        onError={handleImageError}
+      />
+      <h3>Xamarin</h3>
+      <section>An extensive MAUI Course in C#</section>
+      <div className='buttons'>
+      <Button
+        className="but"
+        content="Preview Course"
+        link="http://localhost:3000/signup"
+        status='action'
+        />
+        <Button
+        className="but"
+        content="Purchase Course"
+        link="http://localhost:3000/signup"
+        status='purchase'
+         />
+
+      </div>
+    </div>
+  );
 }
 
 export default CourseCard;
--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -3,18 +3,37 @@ import './home.css'
 import Logo from '../Logo/Logo';
 import CourseCard from '../Courses/Course';
 import IMG from "../../assets/Images/Fail.jpg"
+import AvailableCorses from '../AvailableCourses/AvailableCourses'
 
 function Home(props) {
     return (
-        <>
+        <div className='home'>
            <nav className='bar'>
             <Logo flex='row' />
             <span>
                 Hello Profile owner name
             </span>
            </nav>
-           <CourseCard image={IMG}></CourseCard>
-        </>
+           <h2 className='header'>Owned Courses:</h2>
+           <div className='grid'>
+                <AvailableCorses image={IMG} />
+                <AvailableCorses image={IMG} />
+                <AvailableCorses image={IMG} />
+                <AvailableCorses image={IMG} />
+                <AvailableCorses image={IMG} />
+                <AvailableCorses image={IMG} />
+           </div>
+           <h2 className='header'>Suggested Courses:</h2>
+           <div className='grid'>
+                <CourseCard image={IMG}></CourseCard>
+                <CourseCard image={IMG}></CourseCard>
+                <CourseCard image={IMG}></CourseCard>
+                <CourseCard image={IMG}></CourseCard>
+                <CourseCard image={IMG}></CourseCard>
+                <CourseCard image={IMG}></CourseCard>
+           </div>
+           
+        </div>
     );
 }
 
